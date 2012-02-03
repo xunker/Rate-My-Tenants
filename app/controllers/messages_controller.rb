5d754f55d3651842a7eb729a52f8735cb8d5c5af
Matthew Nielsen <xunker@pyxidis.org>
2012-02-03 15:41:24 -0700

    Message list, view message, uupdate message read state

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,8 @@ class MessagesController < ApplicationController
 	end
 
 	def show
+		@message = Message.find_by_id_and_to_user_id(params[:id], current_user.id) or raise ActiveRecord::RecordNotFound
+		@message.read! if @message.unread?
 	end
 
 	def new
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
 	belongs_to :from_user, :class_name => "User"
 	belongs_to :to_user, :class_name => "User"
+	belongs_to :message
 	belongs_to :review
 
 	named_scope :read, :conditions => "read_at IS NOT NULL"
@@ -12,5 +13,17 @@ class Message < ActiveRecord::Base
 
 	def read?
 		read_at.present?
+	end
+
+	def unread?
+		!read?
+	end
+
+	def read!
+		update_attributes(:read_at => Time.now)
+	end
+
+	def reply?
+		message_id.present?
 	end
 end
